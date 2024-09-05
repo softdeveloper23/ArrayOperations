7cfb4d72aefc7a3e4fd8b31fd645f6b5157a1794
softdeveloper23 <brannongarrett77@gmail.com>
2024-09-05 18:20:26 +0200

    Finished exercise

--- src/main/java/ArrayOperations.java
+++ src/main/java/ArrayOperations.java
@@ -1,14 +1,30 @@
-import java.util.Scanner;
-
 class ArrayOperations {
+    public static void main(String[] args) {
+        int[][] twoDArray = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        printCorners(twoDArray);
+    }
     public static void printCorners(int[][] twoDimArray) {
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextInt()) {
-            for (int i = 0; i < twoDimArray.length; i++) {
-                for (int j = 0; j < twoDimArray[i].length; j++)
-                    twoDimArray[i][j] = scanner.nextInt();
-            }
+        if (twoDimArray == null || twoDimArray.length == 0 || twoDimArray[0].length == 0) {
+            return;
         }
-        // Logic code goes here
+
+        int rows = twoDimArray.length;
+        int cols = twoDimArray[0].length;
+
+        // Top-left corner
+        System.out.print(twoDimArray[0][0] + " ");
+
+        // Top-right corner
+        System.out.println(twoDimArray[0][cols - 1]);
+
+        // Bottom-left corner
+        System.out.print(twoDimArray[rows - 1][0] + " ");
+
+        // Bottom-right corner
+        System.out.println(twoDimArray[rows - 1][cols - 1]);
     }
 }
